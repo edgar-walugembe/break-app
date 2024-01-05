d33c916c030e7feacaf347651dff077d9b788e74
windows <edgar.walugembe.upti@gmail.com>
2024-01-05 17:55:33 +0300

    breakApp 0001

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/users", function (req, res, next) {
+  res.send("respond with a resource 0000");
 });
 
 module.exports = router;
