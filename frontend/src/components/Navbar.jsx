7d72def5334f5a799d709f618a2b30d6eaf425ab
windows <edgar.walugembe.upti@gmail.com>
2024-01-15 19:23:35 +0300

    breakApp 0003

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { React, useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { IoSettings } from "react-icons/io5";
 import { Link } from "react-router-dom";
-import { Navbar } from "./Navbar";
+import Notification from "./Notification";
 
 const Navbar = () => {
   const date = new Date();
@@ -21,11 +21,11 @@ const Navbar = () => {
         <p className="font-semibold">{formattedDate}</p>
       </div>
       <div className="flex gap-5 items-center">
-        <Navbar />
-
         <Link to="/home/user_account">
           <FaUser className="w-[32px] h-[32px] text-yellow-300" />
         </Link>
+
+        <Notification />
 
         <Link to="/home/user_settings">
           <IoSettings className="w-[32px] h-[32px] text-yellow-300" />
