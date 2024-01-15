939a81634d5d24f0696dcffe71ce471db1f38ea6
windows <edgar.walugembe.upti@gmail.com>
2024-01-15 19:15:21 +0300

    breakApp 0003

--- frontend/src/components/Notification.jsx
+++ frontend/src/components/Notification.jsx
@@ -1,7 +1,22 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+import {
+  NovuProvider,
+  PopoverNotificationCenter,
+  NotificationBell,
+} from "@novu/notification-center";
 
 const Notification = () => {
-  return <div>Notification</div>;
+  return (
+    <NovuProvider
+      subscriberId={"on-boarding-subscriber-id-123"}
+      applicationIdentifier={"eVDxcxY37S_g"}
+    >
+      <PopoverNotificationCenter colorScheme={"light"}>
+        {({ unseenCount }) => <NotificationBell unseenCount={unseenCount} />}
+      </PopoverNotificationCenter>
+    </NovuProvider>
+  );
 };
 
 export default Notification;
