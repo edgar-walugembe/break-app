a1896ae615ac9728e5fdb805f7695b81d36bc8b4
windows <edgar.walugembe.upti@gmail.com>
2024-01-18 17:36:06 +0300

    breakApp 0003

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,20 +13,20 @@ import {
 
 const App = () => {
   return (
-    <div className="font-poppins bg-primary w-full overflow-hidden">
+    <div className="font-poppins bg-black  w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
         </div>
       </div>
 
-      <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={` ${styles.flexStart} bg-black`}>
         <div className={`${styles.boxWidth}`}>
           <Hero />
         </div>
       </div>
 
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.paddingX} ${styles.flexStart} bg-black`}>
         <div className={`${styles.boxWidth}`}>
           <Menu />
           <Services />
