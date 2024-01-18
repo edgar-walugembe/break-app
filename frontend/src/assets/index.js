ee198a303a7a3859e5c3775ba233944ef8346f3e
windows <edgar.walugembe.upti@gmail.com>
2024-01-18 17:15:40 +0300

    breakApp 0003

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,12 +1,12 @@
 import banana from "./bananas.jpg";
-import logo from "./break_logo.jpeg";
+import cakes from "./halfcakes.jpeg";
+import cakes00 from "./halfcakes00.jpeg";
 import cassava from "./cassava.jpg";
 import chapati from "./chapati.jpg";
 import chapati00 from "./chapati00.jpg";
 import close from "./close.svg";
 import eggs from "./eggs.jpg";
-import cakes from "./halfcakes.jpeg";
-import cakes00 from "./halfcakes00.jpeg";
+import logo from "./logo.svg";
 import menu from "./menu.svg";
 import odyssey_logo from "./odyssey.png";
 import pani from "./pancakes.jpeg";
@@ -16,4 +16,22 @@ import samosa from "./samosa.jpg";
 import samosa00 from "./samosa00.jpg";
 import sausage from "./sausages.jpg";
 
-export { close, menu };
+export {
+  banana,
+  cakes,
+  cakes00,
+  cassava,
+  chapati,
+  chapati00,
+  close,
+  eggs,
+  logo,
+  menu,
+  odyssey_logo,
+  pani,
+  pani00,
+  rolex,
+  samosa,
+  samosa00,
+  sausage,
+};
--- frontend/src/components/userAppComponents/Navbar.jsx
+++ frontend/src/components/userAppComponents/Navbar.jsx
@@ -1,15 +1,75 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 // import styles, { layout } from "../../style";
-import { close, menu } from "../../assets";
+import { close, logo, menu } from "../../assets";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="breakApp_logo" className="" />
+      <img src={logo} alt="breakApp_logo" className="w-[124px] h-[32px]" />
 
-      <ul className="list-none sm:flex hidden justify-end items-center flex-1"></ul>
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        <li
+          className={`font-poppins font-normal cursor-pointer text-[16px] text-white mr-10`}
+        >
+          me
+        </li>
+        <li
+          className={`font-poppins font-normal cursor-pointer text-[16px] text-white mr-10`}
+        >
+          you
+        </li>
+        <li
+          className={`font-poppins font-normal cursor-pointer text-[16px] text-white mr-10`}
+        >
+          them
+        </li>
+        <li
+          className={`font-poppins font-normal cursor-pointer text-[16px] text-white mr-0`}
+        >
+          us
+        </li>
+      </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute my-4 mx-2 top-20 right-0 rounded-xl min-w-[140px] sidebar`}
+        >
+          <ul className="list-nome flex flex-col justify-end items-center flex-1">
+            <li
+              className={`font-poppins font-normal cursor-pointer text-[16px] text-white mb-4`}
+            >
+              me
+            </li>
+            <li
+              className={`font-poppins font-normal cursor-pointer text-[16px] text-white mb-4`}
+            >
+              you
+            </li>
+            <li
+              className={`font-poppins font-normal cursor-pointer text-[16px] text-white mb-4`}
+            >
+              them
+            </li>
+            <li
+              className={`font-poppins font-normal cursor-pointer text-[16px] text-white mb-0`}
+            >
+              us
+            </li>
+            ;
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
