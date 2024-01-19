667569bcc9ebdf3ea7a095aa1f6f0509cefcde61
windows <edgar.walugembe.upti@gmail.com>
2024-01-19 12:57:36 +0300

    breakApp 0003

--- frontend/src/components/userAppComponents/Hero.jsx
+++ frontend/src/components/userAppComponents/Hero.jsx
@@ -2,12 +2,30 @@
 import React from "react";
 import styles from "../../style";
 
+//icons
+import { GiCash } from "react-icons/gi";
+
 const Hero = () => {
   return (
     <section
       id="home"
       className={`flex md:flex-row flex-col ${styles.paddingY}`}
-    ></section>
+    >
+      <div
+        className={`flex-1 flex-col xl:px-0 sm:px-16 px-6 ${styles.flexStart} `}
+      >
+        <div className="flex flex-row items-center py-[6px] px-4 rounded-[10px] mb-2 bg-yellow">
+          <GiCash className="text-white w-[32px] h-[32px]" />
+          <p className={`${styles.paragraph} ml-2 center`}>
+            A daily
+            <span className="text-white"> Budget </span>of UgSh
+            <span className="text-white"> 2000</span>
+          </p>
+        </div>
+        <div className=""></div>
+        <div className=""></div>
+      </div>
+    </section>
   );
 };
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,15 @@
-import { yellow } from "@mui/material/colors";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   mode: "jit",
   theme: {
     extend: {
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
       backgroundColor: {
         yellow: "yellow",
       },
