6f909c84ced4b48e9c0079544826d671b77e50f6
windows <edgar.walugembe.upti@gmail.com>
2024-01-19 17:22:16 +0300

    breakApp 0003

--- frontend/src/components/userAppComponents/Menu.jsx
+++ frontend/src/components/userAppComponents/Menu.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
+//icons
+import { LuPlusCircle } from "react-icons/lu";
+import { MdOutlineExposurePlus1, MdOutlineExposurePlus2 } from "react-icons/md";
+
 const Menu = () => {
   return <div>Menu</div>;
 };
