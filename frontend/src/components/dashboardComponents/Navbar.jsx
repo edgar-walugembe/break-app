d73db58dfb14595f8da7132e412ef68fb6fee0aa
windows <edgar.walugembe.upti@gmail.com>
2024-01-23 17:16:56 +0300

    breakApp 0004

--- frontend/src/components/dashboardComponents/Navbar.jsx
+++ frontend/src/components/dashboardComponents/Navbar.jsx
@@ -1,8 +1,13 @@
 /* eslint-disable no-unused-vars */
 import { React, useState } from "react";
-import { FaUser } from "react-icons/fa";
-import { IoSettings } from "react-icons/io5";
 import { Link } from "react-router-dom";
+
+//icons imports
+import { FaUser, FaUsers } from "react-icons/fa";
+import { IoSettings } from "react-icons/io5";
+import { GiCash } from "react-icons/gi";
+
+//Other imports
 import Notification from "./Notification";
 
 const Navbar = () => {
@@ -15,24 +20,115 @@ const Navbar = () => {
   });
 
   return (
-    <div className="flex justify-between">
-      <div className="flex flex-col">
-        <h5 className="text-blue-900">Hello, Admin</h5>
-        <p className="font-semibold text-blue-500 text-[12px]">
-          {formattedDate}
-        </p>
+    <div className="surface-ground px-2 py-1 md:px-4 lg:px-6 ">
+      <div className="grid">
+        <div className="flex justify-between col-12">
+          <div className="flex flex-col">
+            <h5 className="text-blue-900">Hello, Admin</h5>
+            <p className="font-semibold text-blue-500 text-[12px]">
+              {formattedDate}
+            </p>
+          </div>
+
+          <div className="flex gap-4 items-center">
+            <Link to="/home/user_account">
+              <FaUser className="w-[20px] h-[20px] text-blue-500" />
+            </Link>
+
+            <Notification className="w-[20px] h-[20px]" />
+
+            <Link to="/home/user_settings">
+              <IoSettings className="w-[20px] h-[20px] text-blue-500" />
+            </Link>
+          </div>
+        </div>
       </div>
 
-      <div className="flex gap-4 items-center">
-        <Link to="/home/user_account">
-          <FaUser className="w-[20px] h-[20px] text-blue-500" />
-        </Link>
+      <div className="grid">
+        <div className="col-12 md:col-6 lg:col-3">
+          <div className="surface-card shadow p-3 rounded">
+            <div className="flex justify-between mb-3">
+              <div>
+                <span className="block text-secondary font-medium mb-3">
+                  Orders
+                </span>
+                <div className="text-black font-medium text-xl">152</div>
+              </div>
+              <div
+                className="flex items-center justify-center bg-yellow rounded"
+                style={{ width: "2.5rem", height: "2.5rem" }}
+              >
+                <i className="pi pi-shopping-cart text-blue-500 text-xl"></i>
+              </div>
+            </div>
+            <span className="text-success font-medium">24 new </span>
+            <span className="text-secondary">since last visit</span>
+          </div>
+        </div>
 
-        <Notification className="w-[20px] h-[20px]" />
+        <div className="col-12 md:col-6 lg:col-3">
+          <div className="surface-card shadow p-3 rounded">
+            <div className="flex justify-between mb-3">
+              <div>
+                <span className="block text-secondary font-medium mb-3">
+                  Budget
+                </span>
+                <div className="text-black font-medium text-xl">$2.100</div>
+              </div>
+              <div
+                className="flex items-center justify-center bg-yellow rounded"
+                style={{ width: "2.5rem", height: "2.5rem" }}
+              >
+                <i className="pi pi-map-marker text-orange-500 text-xl"></i>
+              </div>
+            </div>
+            <span className="text-success font-medium">%52+ </span>
+            <span className="text-secondary">since last week</span>
+          </div>
+        </div>
 
-        <Link to="/home/user_settings">
-          <IoSettings className="w-[20px] h-[20px] text-blue-500" />
-        </Link>
+        <div className="col-12 md:col-6 lg:col-3">
+          <div className="surface-card shadow p-3 rounded">
+            <div className="flex justify-between mb-3">
+              <div>
+                <span className="block text-secondary font-medium mb-3">
+                  Users
+                </span>
+                <div className="text-black font-medium text-xl">28441</div>
+              </div>
+              <div
+                className="flex items-center justify-center bg-yellow rounded"
+                style={{ width: "2.5rem", height: "2.5rem" }}
+              >
+                <FaUsers className="text-xl" />
+              </div>
+            </div>
+            <span className="text-success font-medium">520 </span>
+            <span className="text-secondary">newly registered</span>
+          </div>
+        </div>
+
+        <div className="col-12 md:col-6 lg:col-3">
+          <div className="surface-card shadow p-3 rounded">
+            <div className="flex justify-between mb-3">
+              <div>
+                <span className="block text-secondary font-medium mb-3">
+                  Debts
+                </span>
+                <div className="text-black font-medium text-xl">152 Unread</div>
+              </div>
+              <div
+                className="flex items-center justify-center bg-yellow rounded"
+                style={{ width: "2.5rem", height: "2.5rem" }}
+              >
+                {/* <i className="pi pi-cash text-purple-500 text-xl"></i> */}
+                <GiCash className="text-xl" />
+              </div>
+            </div>
+            <span className="text-success font-medium">85 </span>
+            <span className="text-secondary">responded</span>
+          </div>
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/userAppComponents/Navbar.jsx
+++ frontend/src/components/userAppComponents/Navbar.jsx
@@ -2,14 +2,26 @@
 import React, { useState } from "react";
 // import styles, { layout } from "../../style";
 import { close, logo, menu } from "../../assets";
+import { Link } from "react-router-dom";
 
 //icons
 import { FaUser } from "react-icons/fa";
-import { IoIosNotificationsOutline } from "react-icons/io";
-// import { StyleClass } from "primereact/styleclass";
+import { IoSettings } from "react-icons/io5";
+import { GiCash } from "react-icons/gi";
+import { Notification } from "../dashboardComponents";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+
+  const date = new Date();
+  const formattedDate = date.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  const Username = "eedga";
   return (
     // <nav className="w-full flex py-6 justify-between items-center navbar">
     //   <img src={logo} alt="breakApp_logo" className="w-[130px] h-[36px]" />
@@ -83,13 +95,36 @@ const Navbar = () => {
     //   </div>
     // </nav>
 
-    <div className="surface-ground px-4 py-5 md:px-6 lg:px-8 ">
+    <div className="surface-ground px-2 py-1 md:px-4 lg:px-6 ">
+      <div className="grid">
+        <div className="flex justify-between col-12">
+          <div className="flex flex-col">
+            <h5 className="text-blue-900">Hello, {Username}</h5>
+            <p className="font-semibold text-blue-500 text-[12px]">
+              {formattedDate}
+            </p>
+          </div>
+
+          <div className="flex gap-4 items-center">
+            <Link to="/home/user_account">
+              <FaUser className="w-[20px] h-[20px] text-blue-500" />
+            </Link>
+
+            <Notification className="w-[20px] h-[20px]" />
+
+            <Link to="/home/user_settings">
+              <IoSettings className="w-[20px] h-[20px] text-blue-500" />
+            </Link>
+          </div>
+        </div>
+      </div>
+
       <div className="grid">
         <div className="col-12 md:col-6 lg:col-3">
           <div className="surface-card shadow p-3 rounded">
             <div className="flex justify-between mb-3">
               <div>
-                <span className="block text-gray-100 font-medium mb-3">
+                <span className="block text-secondary font-medium mb-3">
                   Orders
                 </span>
                 <div className="text-black font-medium text-xl">152</div>
@@ -101,8 +136,8 @@ const Navbar = () => {
                 <i className="pi pi-shopping-cart text-blue-500 text-xl"></i>
               </div>
             </div>
-            <span className="text-secondary font-medium">24 new </span>
-            <span className="text-gray-500">since last visit</span>
+            <span className="text-success font-medium">24 new </span>
+            <span className="text-secondary">since last visit</span>
           </div>
         </div>
 
@@ -110,8 +145,8 @@ const Navbar = () => {
           <div className="surface-card shadow p-3 rounded">
             <div className="flex justify-between mb-3">
               <div>
-                <span className="block text-gray-500 font-medium mb-3">
-                  Revenue
+                <span className="block text-secondary font-medium mb-3">
+                  Budget
                 </span>
                 <div className="text-black font-medium text-xl">$2.100</div>
               </div>
@@ -122,8 +157,8 @@ const Navbar = () => {
                 <i className="pi pi-map-marker text-orange-500 text-xl"></i>
               </div>
             </div>
-            <span className="text-secondary font-medium">%52+ </span>
-            <span className="text-black">since last week</span>
+            <span className="text-success font-medium">%52+ </span>
+            <span className="text-secondary">since last week</span>
           </div>
         </div>
 
@@ -131,8 +166,8 @@ const Navbar = () => {
           <div className="surface-card shadow p-3 rounded">
             <div className="flex justify-between mb-3">
               <div>
-                <span className="block text-gray-500 font-medium mb-3">
-                  Customers
+                <span className="block text-secondary font-medium mb-3">
+                  Users
                 </span>
                 <div className="text-black font-medium text-xl">28441</div>
               </div>
@@ -143,8 +178,8 @@ const Navbar = () => {
                 <i className="pi pi-inbox text-cyan-500 text-xl"></i>
               </div>
             </div>
-            <span className="text-secondary font-medium">520 </span>
-            <span className="text-black">newly registered</span>
+            <span className="text-success font-medium">520 </span>
+            <span className="text-secondary">newly registered</span>
           </div>
         </div>
 
@@ -152,8 +187,8 @@ const Navbar = () => {
           <div className="surface-card shadow p-3 rounded">
             <div className="flex justify-between mb-3">
               <div>
-                <span className="block text-gray-500 font-medium mb-3">
-                  Comments
+                <span className="block text-secondary font-medium mb-3">
+                  Debts
                 </span>
                 <div className="text-black font-medium text-xl">152 Unread</div>
               </div>
@@ -161,11 +196,12 @@ const Navbar = () => {
                 className="flex items-center justify-center bg-yellow rounded"
                 style={{ width: "2.5rem", height: "2.5rem" }}
               >
-                <i className="pi pi-comment text-purple-500 text-xl"></i>
+                {/* <i className="pi pi-cash text-purple-500 text-xl"></i> */}
+                <GiCash className="text-xl" />
               </div>
             </div>
-            <span className="text-yellow font-medium">85 </span>
-            <span className="text-black">responded</span>
+            <span className="text-success font-medium">85 </span>
+            <span className="text-secondary">responded</span>
           </div>
         </div>
       </div>
