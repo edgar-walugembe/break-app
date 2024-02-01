6f39ebce1c6e8ae9afc9efb83c689f3eb0b31be9
windows <edgar.walugembe.upti@gmail.com>
2024-02-01 13:10:10 +0300

    breakApp 0005

--- frontend/src/components/userAppComponents/FoodMenu.jsx
+++ frontend/src/components/userAppComponents/FoodMenu.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { cards } from "../../constants";
 
 const FoodMenu = () => {
   return (
     <div className="surface-ground px-2 py-1 md:px-4 lg:px-6 ">
-      <div className="grid"></div>
+      <div className="grid">
+        <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
+      </div>
     </div>
   );
 };
