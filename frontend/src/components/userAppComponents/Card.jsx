f1bab5227599c85af547a3daad1ee6c8d1bb6bcc
windows <edgar.walugembe.upti@gmail.com>
2024-02-01 15:12:17 +0300

    breakApp 0005

--- frontend/src/components/userAppComponents/Card.jsx
+++ frontend/src/components/userAppComponents/Card.jsx
@@ -1,32 +1,19 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { cards } from "../../constants";
-import styles from "../../style";
 
-const Card = ({ icon, title, content, index }) => {
+const Card = ({ icon, title, id, price, img }) => {
   return (
-    <div
-      className={`flex flex-row p-6 rounded-[20px] ${
-        index == !cards.length - 1 ? "mb-6" : "mb-0"
-      } feature-card`}
-    >
-      <div
-        className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
-      >
-        <img
-          src={icon}
-          alt={title}
-          className="w-[50%] h-[50%] object-contain"
-        />
+    <div className="card">
+      <img src={img} alt={title} className="card.img" />
+
+      <div className="card.title">
+        <h1>{title}</h1>
       </div>
-      <div className="flex-1 flex flex-col ml-3">
-        <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
-          {title}
-        </h4>
-        <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
-          {content}
-        </p>
+
+      <div className="card.price">
+        <span>{price}</span>
+        <span>{icon}</span>
       </div>
     </div>
   );
--- frontend/src/components/userAppComponents/FoodMenu.jsx
+++ frontend/src/components/userAppComponents/FoodMenu.jsx
@@ -1,15 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { cards } from "../../constants";
+import {} from "../../assets";
+import Card from "./Card";
 
 const FoodMenu = () => {
   return (
     <div className="surface-ground px-2 py-1 md:px-4 lg:px-6 ">
       <div className="grid">
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
+        <h1>Snacks Available</h1>
+
+        {cards.map((item) => (
+          <Card key={item.id} {...item} />
+        ))}
       </div>
     </div>
   );
