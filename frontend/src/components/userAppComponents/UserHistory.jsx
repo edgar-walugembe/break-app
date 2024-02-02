880ef8a08ece8aa470e6de7f2d41d0b18c5bd84c
windows <edgar.walugembe.upti@gmail.com>
2024-02-02 12:13:41 +0300

    breakApp 0005

--- frontend/src/components/userAppComponents/UserHistory.jsx
+++ frontend/src/components/userAppComponents/UserHistory.jsx
@@ -1,17 +1,5 @@
 // /* eslint-disable react-refresh/only-export-components */
 // /* eslint-disable no-unused-vars */
-// import React from "react";
-
-// const UserHistory = () => {
-//   return (
-//     <div className="surface-ground px-2 py-1 md:px-4 lg:px-6 ">
-//       <div className="grid"></div>
-//     </div>
-//   );
-// };
-
-// export default UserHistory;
-
 import * as React from "react";
 import PropTypes from "prop-types";
 import { alpha } from "@mui/material/styles";
