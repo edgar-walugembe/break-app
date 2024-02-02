15cab0cdd9a5478d0382fb5b6a88a11f3db9d3f0
windows <edgar.walugembe.upti@gmail.com>
2024-02-02 12:03:44 +0300

    breakApp 0005

--- frontend/src/components/userAppComponents/Card.jsx
+++ frontend/src/components/userAppComponents/Card.jsx
@@ -6,14 +6,16 @@ import { FiPlusCircle } from "react-icons/fi";
 const Card = ({ title, id, price, img }) => {
   return (
     <div className="col-12 md:col-6 lg:col-4 relative">
-      <div className="card p-2 flex flex-col rounded bg-white">
+      <div className="card flex flex-col rounded bg-white">
         <div className="card.img">
           <img src={img} alt={title} className="w-full h-[200px] rounded-md" />
         </div>
 
-        <h3 className="text-center font-semibold text-[16px]">{title}</h3>
+        <h3 className="text-center font-medium text-[16px]">{title}</h3>
 
-        <span className="text-center text-amber font-medium">Shs.{price}</span>
+        <span className="text-center text-cyan font-medium mx-14 mb-1">
+          Shs.{price}
+        </span>
 
         <div
           className="icon.box absolute mt-0 mr-0 flex items-center justify-center bg-amber rounded"
